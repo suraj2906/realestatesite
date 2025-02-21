c7e1ac2fe49169f75dad5a26344eae03aee01366
suraj2906 <surajshah.bombay@gmail.com>
2025-02-21 13:03:06 +0530

    Fixed config error

--- next.config.ts
+++ next.config.ts
@@ -9,7 +9,7 @@ const nextConfig: NextConfig = {
         protocol: 'https',
         hostname: 'images.pexels.com',
         port: '',
-        pathname: '',
+        pathname: '/',
         search: '',
       },
     ],
